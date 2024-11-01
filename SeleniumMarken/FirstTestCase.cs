fc92105830b16dfb8da0c75a09de106fcff6b95f
Opilane TTHK <opilane@tthk.ee>
2024-11-01 13:11:57 +0200

    COmmit
    Ma võtsin markeni selenium ülesande kus me tegime alguse ära et selenim töötaks, sest ma unustasin enda oma commitida. Testid tegin ise

--- SeleniumMarken/FirstTestCase.cs
+++ SeleniumMarken/FirstTestCase.cs
@@ -8,12 +8,17 @@ namespace SeleniumMarken
     {
         static void Main(string[] args)
         {
-            //TestHidingButton();
-            //TestClickTwice();
-            TestHiddenButtons();
+            //TestClick(); // Working/Pass
+            //TestDouubleClick(); // Working/Pass
+            //TestHiddenLayers(); // Working/Pass
+            //TestClienntDelay(); // Working/Pass
+            //Testloaddelays();  //Working/Pass
+            //TestUpload();  //Working/Pass
+            //TestHidingButton(); //Working/Pass
+
         }
 
-        private static void TestHidingButton()
+        private static void TestClick()
         {
             string binaryLocation = "C:\\Users\\opilane\\source\\repos\\FFP\\FirefoxPortable\\App\\Firefox64\\Firefox.exe";
 
@@ -21,13 +26,13 @@ namespace SeleniumMarken
             options.BrowserExecutableLocation = binaryLocation;
 
             IWebDriver driver = new FirefoxDriver("C:\\Users\\opilane\\source\\repos\\SeleniumMarken\\SeleniumMarken\\drivers", options);
-            driver.Url = "http://www.uitestingplayground.com/scrollbars";
+            driver.Url = "http://www.uitestingplayground.com/click";
 
-            IWebElement element = driver.FindElement(By.Id("hidingButton"));
+            IWebElement element = driver.FindElement(By.Id("badButton"));
             element.Click();
         }
 
-        private static void TestClickTwice()
+        private static void TestDouubleClick()
         {
             string binaryLocation = "C:\\Users\\opilane\\source\\repos\\FFP\\FirefoxPortable\\App\\Firefox64\\Firefox.exe";
 
@@ -43,7 +48,7 @@ namespace SeleniumMarken
 
         }
 
-        private static void TestHiddenButtons()
+        private static void TestHiddenLayers()
         {
             string binaryLocation = "C:\\Users\\opilane\\source\\repos\\FFP\\FirefoxPortable\\App\\Firefox64\\Firefox.exe";
 
@@ -51,13 +56,71 @@ namespace SeleniumMarken
             options.BrowserExecutableLocation = binaryLocation;
 
             IWebDriver driver = new FirefoxDriver("C:\\Users\\opilane\\source\\repos\\SeleniumMarken\\SeleniumMarken\\drivers", options);
-            driver.Url = "http://www.uitestingplayground.com/visibility";
+            driver.Url = "http://www.uitestingplayground.com/hiddenlayers";
 
-            IWebElement element = driver.FindElement(By.Id("hideButton"));
+            IWebElement element = driver.FindElement(By.Id("greenButton"));
             element.Click();
-            IWebElement element2 = driver.FindElement(By.Id("removedButton"));
-            element.Click();
+        }
 
+        private static void TestClienntDelay()
+        {
+            string binaryLocation = "C:\\Users\\opilane\\source\\repos\\FFP\\FirefoxPortable\\App\\Firefox64\\Firefox.exe";
+
+            FirefoxOptions options = new FirefoxOptions();
+            options.BrowserExecutableLocation = binaryLocation;
+
+            IWebDriver driver = new FirefoxDriver("C:\\Users\\opilane\\source\\repos\\SeleniumMarken\\SeleniumMarken\\drivers", options);
+            driver.Url = "http://www.uitestingplayground.com/clientdelay";
+
+            IWebElement element = driver.FindElement(By.Id("ajaxButton"));
+            element.Click();
+            Thread.Sleep(15000);
+            IWebElement p = driver.FindElement(By.ClassName("bg-success"));
+            p.Click();
+        }
+
+        private static void Testloaddelays()
+        {
+            string binaryLocation = "C:\\Users\\opilane\\source\\repos\\FFP\\FirefoxPortable\\App\\Firefox64\\Firefox.exe";
+
+            FirefoxOptions options = new FirefoxOptions();
+            options.BrowserExecutableLocation = binaryLocation;
+
+            IWebDriver driver = new FirefoxDriver("C:\\Users\\opilane\\source\\repos\\SeleniumMarken\\SeleniumMarken\\drivers", options);
+            driver.Url = "http://www.uitestingplayground.com/";
+
+            IWebElement element = driver.FindElement(By.LinkText("Load Delay"));
+            element.Click();
+        }
+
+        private static void TestUpload()
+        {
+            string binaryLocation = "C:\\Users\\opilane\\source\\repos\\FFP\\FirefoxPortable\\App\\Firefox64\\Firefox.exe";
+
+            FirefoxOptions options = new FirefoxOptions();
+            options.BrowserExecutableLocation = binaryLocation;
+
+            IWebDriver driver = new FirefoxDriver("C:\\Users\\opilane\\source\\repos\\SeleniumMarken\\SeleniumMarken\\drivers", options);
+            driver.Url = "http://www.uitestingplayground.com/upload";
+
+            IWebElement fileInput = driver.FindElement(By.Id("file-upload"));
+            string filePath = @"C:\path\to\your\file.txt";
+            fileInput.SendKeys(filePath);
+
+        }
+
+        private static void TestHidingButton()
+        {
+            string binaryLocation = "C:\\Users\\opilane\\source\\repos\\FFP\\FirefoxPortable\\App\\Firefox64\\Firefox.exe";
+
+            FirefoxOptions options = new FirefoxOptions();
+            options.BrowserExecutableLocation = binaryLocation;
+
+            IWebDriver driver = new FirefoxDriver("C:\\Users\\opilane\\source\\repos\\SeleniumMarken\\SeleniumMarken\\drivers", options);
+            driver.Url = "http://www.uitestingplayground.com/scrollbars";
+
+            IWebElement element = driver.FindElement(By.Id("hidingButton"));
+            element.Click();
         }
     }
 }
